e3ab2a395bcafb05e4eaadd3cbf34d42e575e7f8
Shirley940408 <33606914+Shirley940408@users.noreply.github.com>
2018-08-04 23:09:47 -0400

    Add files via upload

--- single.php
+++ single.php
@@ -1,11 +1,7 @@
 <?php 
 require_once './vendor/autoload.php';
-
-
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader, array());
 
-echo $twig->render('base.html', array('name' => 'Fabien'));
-
-
+echo $twig->render('single.html', array('name' => 'Fabien'));
  ?>
